179b0d0b9d4f41e8781d3959cddd12d0362982dc
henrygh1981 <85414973+henrygh1981@users.noreply.github.com>
2021-07-12 23:31:47 -0700

    moving the download button

--- js/index.js
+++ js/index.js
@@ -6,18 +6,21 @@ $("#download_button").mouseenter(function() {
 });
 
 $(document).ready(function(){
-    /*
-    $("#download_button").animate({
-        width: "70%",
-        opacity: 0.9,
-        fontSize: "2em",
+    $("#download_button_div").show();
+    $("#download_button_div").animate({
+        bottom: "0px",
       }, 1000, function() {
         // Animation complete.
     });
-    */
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%'
+    });
+    /*
     $("#download_button").mouseenter(function(){
         // alert("You entered button!");
         // $(".download-button").fadeToggle("slow");
         $(".download-button").attr('style', 'background-color: blue !important');
     });
+    */
 });
